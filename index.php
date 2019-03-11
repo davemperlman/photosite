a4c7bdda727439d411c32c28992b9c88df235f3e
Dave Perlman <daveperlman89@me.com>
2019-03-11 15:40:56 -0400

    recent pdates

--- index.php
+++ index.php
@@ -6,12 +6,32 @@
 		
 	</div>
 
+	<div class="bg bg-arch hidden">
+		
+	</div>
+
+	<div class="bg bg-people hidden">
+		
+	</div>
+
+	<div class="bg contact hidden">
+		
+	</div>
+
 	<div class="menu">
 		<ul>
-			<li><a href="#">Dark</a></li>
-			<li><a href="#">RE</a></li>
-			<li><a href="#">People</a></li>
-			<li><a href="#">Contact</a></li>
+			<li><a data-target="_css/arch.css" href="#">
+				<i class="fas fa-moon"></i>
+			</a></li>
+			<li><a data-target=".bg-arch" href="#">
+				<i class="fas fa-building"></i>
+			</a></li>
+			<li><a data-target=".bg-people" href="#">
+				<i class="fas fa-users"></i>
+			</a></li>
+			<li><a href="#">
+				<i class="fas fa-envelope"></i>
+			</a></li>
 		</ul>
 	</div>
 	
@@ -21,6 +41,19 @@
 			<img class="carousel-item" src="_img/dark/<?php echo $key ?>" alt="">
 		<?php endforeach ?>
 	</div>
+
+
+	<!-- JS -->
+
+	<script>
+		$('a').click( function(event) {
+			event.preventDefault();
+			$('a').removeClass('active');
+			$(this).addClass('active');
+			target = $(this).data('target');
+			$('#colorpicker').attr('href', target);
+		});
+	</script>
 
 <?php 
 	require_once 'parts/footer.php';
--- parts/footer.php
+++ parts/footer.php
@@ -13,7 +13,7 @@
 		</ul>
 	</footer>
 </body>
-<script>
+<!-- <script>
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
@@ -25,5 +25,6 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
-</script>
+</script> -->
+
 </html>
