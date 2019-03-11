5b4cd831e4e5d6044fca849dba19604a04b2253b
Dave Perlman <daveperlman89@me.com>
2019-03-11 13:27:59 -0400

    first commit of new branch

--- index.php
+++ index.php
@@ -6,6 +6,21 @@
 		
 	</div>
 
+	<div class="menu">
+		<ul>
+			<li><a href="#">Dark</a></li>
+			<li><a href="#">RE</a></li>
+			<li><a href="#">People</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+	</div>
+	
+	<div class="carousel">
+		<?php $files = scandir('_img/dark'); ?>
+		<?php foreach ($files as $key): ?>
+			<img class="carousel-item" src="_img/dark/<?php echo $key ?>" alt="">
+		<?php endforeach ?>
+	</div>
 
 <?php 
 	require_once 'parts/footer.php';
--- parts/header.php
+++ parts/header.php
@@ -9,7 +9,7 @@
 	<link rel="stylesheet" href="_css/main.css">
 </head>
 <body id="main">
-	<div id="mySidenav" class="sidenav">
+	<!-- <div id="mySidenav" class="sidenav">
 	  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
 	  <a href="#">Real Estate</a>
 	  <a href="#">Landscape</a>
@@ -17,10 +17,10 @@
 	  <a href="#">Contact</a>
 	</div>
 
-	<!-- Use any element to open the sidenav -->
+	Use any element to open the sidenav
 	<span id="menu" onclick="openNav()">&#9776;</span>
 
-	<!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page -->
+	<!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page --> -->
 	<div class="brand">
 		Dave Perlman
 	</div>
